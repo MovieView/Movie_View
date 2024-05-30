eb22a4edcd97852215184e35265d1f605412da6a
Ud293nBqda@outlook.kr <johndoe@example.com>
2024-05-30 22:12:32 +0900

    [Modify] Redis 모듈 수정
    Refactor: "Redis Client 객체 생성 방법 수정"
    
    Redis 접근 병행성을 고려하여 각 요청에서 따로 Client 생성 후 변수에 전달

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -6,34 +6,27 @@ const client = createClient({
   url: process.env.REDIS_URL
 });
 
-client.on('connect', () => {
-  console.log('Connected to Redis');
-});
-
-client.on('error', (error) => {
-  console.error(error);
-});
-
-
 export const setRedisData = async (
+  redisClient : typeof client,
   key: string, 
   value: string,
   options: Object = {}
 ) => {
-  await client.connect();
-  await client.set(key, value, options);
-  await client.quit();
+  await redisClient.connect();
+  await redisClient.set(key, value, options);
+  await redisClient.quit();
 };
 
-export const getRedisData = async (key: string) => {
-  await client.connect();
+export const getRedisData = async (
+    redisClient : typeof client,
+    key: string
+) => {
+  await redisClient.connect();
   try {
-    return await client.get(key);
+    return await redisClient.get(key);
   } catch (error) {
     return null;
   } finally {
-    await client.quit();
+    await redisClient.quit();
   }
 };
-
-export default client;
