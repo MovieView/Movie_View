d6054bca292c96198994abd4ceee2d37f197b00b
jeong922 <hwajeong922@naver.com>
2024-06-02 20:54:39 +0900

    feat: id별 리뷰 추가, 수정, 삭제 API 구현

---
+++ src/app/api/review/[reviewId]/route.ts
@@ -0,0 +1,135 @@
+import { db } from '@/app/db/db';
+import { IReviewData } from '../route';
+import { FieldPacket, RowDataPacket } from 'mysql2';
+
+// id별 리뷰 삭제
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { reviewId: number } }
+) {
+  try {
+    // TODO: userId 받아오기 구현 필요
+    const userId = '1';
+
+    if (!userId) {
+      return new Response('Authentication Error', { status: 401 });
+    }
+
+    const review = await getReviewById(params.reviewId, userId);
+
+    if (!review) {
+      return new Response(
+        JSON.stringify({ message: 'Review does not exist.' }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    await deleteReview(params.reviewId, userId);
+
+    return new Response(
+      JSON.stringify({ message: 'Review has been deleted.' }),
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    // TODO: 에러처리
+    return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
+      status: 500,
+    });
+  }
+}
+
+// id별 리뷰 수정
+export async function PUT(
+  req: Request,
+  { params }: { params: { reviewId: number } }
+) {
+  try {
+    // TODO: userId 받아오기 구현 필요
+    const userId = '1';
+
+    if (!userId) {
+      return new Response('Authentication Error', { status: 401 });
+    }
+
+    const review = await getReviewById(params.reviewId, userId);
+
+    if (!review) {
+      return new Response(
+        JSON.stringify({ message: 'Review does not exist.' }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const data: IReviewData = await req.json();
+
+    await updateReview(params.reviewId, userId, data);
+    return new Response(
+      JSON.stringify({ message: 'Review has been updated.' }),
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    // TODO: 에러처리
+    return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
+      status: 500,
+    });
+  }
+}
+
+async function getReviewById(reviewId: number, userId: string) {
+  const sql = `SELECT * FROM reviews WHERE id=UNHEX(?) AND users_id=? `;
+  const values: Array<string | number> = [reviewId, parseInt(userId)];
+  try {
+    const [result]: [RowDataPacket[], FieldPacket[]] = await db
+      .promise()
+      .execute(sql, values);
+    return result[0];
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function deleteReview(reviewId: number, userId: string) {
+  const sql = `DELETE FROM reviews WHERE id=UNHEX(?) AND users_id=? `;
+  const values: Array<string | number> = [reviewId, parseInt(userId)];
+
+  try {
+    const [result] = await db.promise().query(sql, values);
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function updateReview(
+  reviewId: number,
+  userId: string,
+  data: IReviewData
+) {
+  const sql = `UPDATE reviews SET rating=?, content=? WHERE id=UNHEX(?) AND users_id=? `;
+  const values: Array<string | number> = [
+    data.rating,
+    data.content,
+    reviewId,
+    parseInt(userId),
+  ];
+
+  try {
+    const [result] = await db.promise().query(sql, values);
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
---
+++ src/app/api/review/route.ts
@@ -0,0 +1,67 @@
+import { db } from '@/app/db/db';
+import { ResultSetHeader } from 'mysql2';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface IReviewData {
+  movies_id: number;
+  title: string;
+  rating: number;
+  content: string;
+}
+
+// 리뷰 작성
+export async function POST(req: Request) {
+  try {
+    // TODO: userId 받아오기 구현 필요
+    const userId = '1';
+
+    if (!userId) {
+      return new Response('Authentication Error', { status: 401 });
+    }
+
+    const data: IReviewData = await req.json();
+
+    const review = await addReview(userId, data);
+    //TODO: movies 테이블에 데이터 movies_id 저장 구현
+    if (!review) {
+      return new Response(JSON.stringify({ message: 'Bad Request' }), {
+        status: 400,
+      });
+    }
+
+    return new Response(
+      JSON.stringify({ message: 'Review has been created.' }),
+      {
+        status: 201,
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    // TODO: 에러처리
+    return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
+      status: 500,
+    });
+  }
+}
+
+async function addReview(userId: string, data: IReviewData) {
+  const id = uuidv4().replace(/-/g, '');
+  const sql = `INSERT INTO reviews (id, movies_id, users_id, rating, title, content) VALUES(UNHEX(?), ?, ?, ?, ?, ?)`;
+  const values = [
+    id,
+    data.movies_id,
+    parseInt(userId),
+    data.rating,
+    data.title,
+    data.content,
+  ];
+
+  try {
+    const [result] = await db.promise().query(sql, values);
+
+    return (result as ResultSetHeader).affectedRows;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
