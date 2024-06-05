3926cbc56484b405a5267f6501dabfb767a9d9d7
jeong922 <hwajeong922@naver.com>
2024-06-05 19:09:37 +0900

    feat: 영화별 리뷰 목록 조회 API 정렬 기능 추가

--- src/app/api/movie/[movieId]/review/route.ts
+++ src/app/api/movie/[movieId]/review/route.ts
@@ -4,6 +4,7 @@ import { FieldPacket, RowDataPacket } from 'mysql2';
 // 영화별 리뷰 조회
 const MAX_RESULT = 8;
 const PAGE = 1;
+const SORT = 'latest';
 
 export async function GET(
   req: Request,
@@ -13,11 +14,13 @@ export async function GET(
     const { searchParams } = new URL(req.url);
     let maxResults = searchParams.get('maxResults') ?? MAX_RESULT;
     let page = searchParams.get('page') ?? PAGE;
+    let sort = searchParams.get('sort') ?? SORT;
 
     const reviews = await getReviews(
       params.movieId,
       Number(maxResults),
-      Number(page)
+      Number(page),
+      sort
     );
 
     const count = await reviewsCount(params.movieId);
@@ -31,16 +34,22 @@ export async function GET(
     return Response.json(result);
   } catch (err) {
     console.error(err);
-    // TODO: 에러처리
     return new Response(JSON.stringify({ message: 'Internal Server Error' }), {
       status: 500,
     });
   }
 }
 
-// TODO: 정렬 방식 수정 필요
-async function getReviews(movieId: number, maxResults: number, page: number) {
+async function getReviews(
+  movieId: number,
+  maxResults: number,
+  page: number,
+  sort: string
+) {
   const offset = maxResults * (page - 1);
+  const values: Array<string | number> = [movieId, offset, maxResults];
+  const orderBy =
+    sort === 'like' ? 'likes DESC, createdAt DESC' : 'createdAt DESC';
   const sql = `SELECT HEX(r.id) AS id, r.movies_id AS movieId, r.users_id AS userId, r.rating, r.title,
                 r.content, r.created_at AS createdAt, r.updated_at AS updatedAt, u.nickname, upp.filepath,
                 (SELECT COUNT(*) FROM reviews_likes AS rl WHERE HEX(rl.reviews_id) = HEX(r.id)) AS likes
@@ -48,9 +57,8 @@ async function getReviews(movieId: number, maxResults: number, page: number) {
                 JOIN users AS u ON u.id = r.users_id
                 LEFT JOIN users_profile_pictures AS upp ON u.id=upp.users_id
                 WHERE r.movies_id=?
-                ORDER BY updatedAt
+                ORDER BY ${orderBy}
                 LIMIT ?, ?`;
-  const values: Array<string | number> = [movieId, offset, maxResults];
   try {
     const [result] = await db.promise().query(sql, values);
     return result;
