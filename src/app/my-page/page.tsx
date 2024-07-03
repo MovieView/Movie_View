cecfcfad8e0b228eee4c9d2dc5244e1633533b6a
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-07-03 18:41:15 +0700

    style: 좋아요 목록 리스트로 이동하는 아이콘, 상위 title 지정

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Edit from '@/components/myPage/Edit';
+import Edit from '@/components/my-page/Edit';
 export default function MyPage(){
     return (
         <div className='flex justify-center items-center min-h-screen overflow-y-hidden'>
--- src/components/my-page/Edit.tsx
+++ src/components/my-page/Edit.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useState } from 'react';
 import { useSession } from 'next-auth/react';
-
+import { FaHeart } from 'react-icons/fa6';
+import Link from 'next/link';
 const Edit = () => {
     const { data: session } = useSession();
     const [selectedImage, setSelectedImage] = useState<File | null>(null);
@@ -25,19 +26,22 @@ const Edit = () => {
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (selectedImage || nickname) {
-            // 이미지 업로드 처리 로직 추가 (예: 서버로 업로드)
-            console.log('Image uploaded:', selectedImage);
-            console.log('Nickname:', nickname);
+            // 서버로 보내는 로직
         }
+    };
+    const heartList = () => {
+        // 좋아요 한 리스트로 이동
     };
 
     if (!session) {
-        return <div>로그인해주세요.</div>;
+        return <div>먼저 로그인해주세요.</div>;
     }
 
     return (
-        <div className='bg-gray-100 w-2/5 h-auto text-gray-600 rounded-lg mb-20 p-20'>
+        // <div className='bg-first w-2/5 h-auto text-black-600 rounded-lg mb-20 pb-10'>
+        <div className='bg-gray-100 w-2/5 h-auto text-black-600 rounded-lg mb-20 pb-10 border border-first'>
             <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center space-y-4'>
+            <div className='p-10 text-[20px] font-bold'>프로필 수정<FaHeart className='relative bottom-6 left-80 text-first'  onClick={heartList}/></div>
                 <div className="relative flex justify-center items-center w-32 h-32 rounded-full border-2 border border-gray-400 cursor-pointer">
                     <input 
                         type='file' 
