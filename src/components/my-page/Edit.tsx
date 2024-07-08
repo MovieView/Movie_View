f50276a5931620775e6d14199728a221f91b5fff
Ntpc_Week2 <munsoyeong12@gmail.com>
2024-07-08 15:45:07 +0700

    style: 하트 아이콘 위치 변경

--- src/components/my-page/Edit.tsx
+++ src/components/my-page/Edit.tsx
@@ -1,11 +1,11 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { FaHeart } from 'react-icons/fa6';
-import Image from 'next/image';
 
 const updateProfile = async (userId: string, profilePicture?: File, userName?: string) => {
   const formData = new FormData();
+
   formData.append('userId', userId);
   if (profilePicture) {
     formData.append('profilePicture', profilePicture);
@@ -69,10 +69,14 @@ const Edit = () => {
   return (
     <div className='bg-gray-100 w-2/5 h-auto text-black-600 rounded-lg mb-20 pb-10 border border-first'>
       <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center space-y-4'>
-        <div className='p-10 text-[20px] font-bold'>
-          프로필 수정
-          <FaHeart className='relative bottom-6 left-80 text-first' onClick={heartList} />
+      <div className='w-full flex justify-center'>
+        <div className='relative w-full max-w-4xl p-10 text-[20px] font-bold flex items-center justify-between'>
+          <span className='mx-auto'>프로필 수정</span>
+          <FaHeart className='absolute right-[20px] text-first' onClick={heartList} />
         </div>
+      </div>
+
+        
         <div className='relative flex justify-center items-center w-32 h-32 rounded-full border-2 border border-gray-400 cursor-pointer'>
           <input
             type='file'
@@ -81,9 +85,9 @@ const Edit = () => {
             className='absolute inset-0 opacity-0 cursor-pointer'
           />
           {preview ? (
-            <Image src={preview} alt='Preview' layout="fill" objectFit="cover" className='rounded-full' />
+            <img src={preview} alt='Preview' layout="fill" objectFit="cover" className='rounded-full' />
           ) : (
-            <Image src={session.user?.image || '/default-profile.png'} alt='Profile' layout="fill" objectFit="cover" className='rounded-full' />
+            <img src={session.user?.image || '/default-profile.png'} alt='Profile' layout="fill" objectFit="cover" className='rounded-full' />
           )}
         </div>
         <input
@@ -91,7 +95,7 @@ const Edit = () => {
           placeholder='수정하실 닉네임을 입력해주세요.'
           value={userName}
           onChange={handleUserNameChange}
-          className='w-4/5 h-10 border border-gray rounded p-2'
+          className='w-4/5 h-10  border border-gray rounded p-2'
         />
         <button type='submit' className='px-4 py-2 bg-blue-500 text-white rounded'>
           수정하기
