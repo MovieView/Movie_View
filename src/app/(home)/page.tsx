986c02030d4080e9d5524cb6ee771f343b27f2d8
choongnyeong6215 <choong6215@gmail.com>
2024-07-08 20:57:06 +0900

    feat: 소셜 로그인 프로바이더별 최초 로그인 계정 닉네임 추가 구현

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import clsx from 'clsx';
 
 import SearchMoviePoster from '@/components/home/SearchMoviePoster';
@@ -10,9 +10,11 @@ import LoadingError from '@/components/home/LoadingError';
 import LoadMoreButton from '@/components/home/LoadMoreButton';
 import { isInViewport } from '@/utils/domUtils';
 import useMovieSearch from '@/hooks/useMovieSearch';
+import { useSession } from 'next-auth/react';
 
-export default function Home(): React.ReactElement {
-  const getNextPageButton = React.useRef<HTMLButtonElement>(null);
+export default function Home() {
+  const getNextPageButton = React.useRef(null);
+
   // 영화 리스트 불러오기
   const {
     searchQuery,
@@ -52,29 +54,89 @@ export default function Home(): React.ReactElement {
 
   // 1초마다 다음 페이지 버튼이 화면에 있는지 확인
   useEffect(() => {
-    const time: NodeJS.Timeout = setInterval(() => {
+    const interval = setInterval(() => {
       if (
         getNextPageButton.current &&
         isInViewport(getNextPageButton.current) &&
         !isFetchingNextPage
       ) {
         fetchNextPage().then(() => {
-          console.log('Fetched next page'); // Debugging
+          console.log('Fetched next page');
         });
       }
     }, 1000);
 
     return () => {
-      clearInterval(time);
+      clearInterval(interval);
     };
-  }, []);
+  }, [fetchNextPage, isFetchingNextPage]);
+
+  // 모달 관련
+  const [showModal, setShowModal] = useState(false);
+  const { data: session } = useSession();
+  const [nickname, setNickname] = useState<string | undefined>(
+    session?.user.name
+  );
+  const modalShownRef = useRef(false);
+
+  const handleNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setNickname(e.target.value);
+  };
+
+  const handleUpdateNickname = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const userId = session?.uid;
+      const provider = session?.provider;
+
+      const response = await fetch('/api/nickname', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nickname, userId, provider }),
+      });
+
+      if (response.ok) {
+        localStorage.setItem(`nicknameUpdated_${provider}`, 'true');
+        setShowModal(false);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  // 최초 로그인 시에만 닉네임 설정
+  useEffect(() => {
+    const provider = session?.provider;
+    const nicknameUpdated = localStorage.getItem(`nicknameUpdated_${provider}`);
+    if (
+      session &&
+      session?.user &&
+      !nicknameUpdated &&
+      !modalShownRef.current
+    ) {
+      modalShownRef.current = true;
+      openModal();
+    }
+  }, [session]);
 
   return (
     <div className='w-full bg-white relative flex flex-col grow'>
       <div className='flex flex-col w-full grow'>
         {/* 검색 바 표시하기 */}
-        {(!isError && !isLoadingError && !isRefetchError) && (
-          <SearchBar 
+        {!isError && !isLoadingError && !isRefetchError && (
+          <SearchBar
             searchBarStyle={searchBarStyle}
             searchQuery={searchQuery}
             handleQueryChange={handleQueryChange}
@@ -82,15 +144,13 @@ export default function Home(): React.ReactElement {
           />
         )}
         {/* 로딩 중일 경우 */}
-        {(isLoading || isRefetching || isPending) && (
-          <LoadingPing />
-        )}
+        {(isLoading || isRefetching || isPending) && <LoadingPing />}
         {/* 에러 발생 시 */}
         {(isError || isRefetchError || isLoadingError) && (
           <LoadingError refetch={refetch} />
         )}
         {/* 영화 포스터 표시하기 */}
-        {(isSuccess && !(isLoading || isRefetching) && data) && (
+        {isSuccess && !(isLoading || isRefetching) && data && (
           <div className='grid w-[90%] md:w-[70%] mx-auto mb-10 grid-cols-2 gap-6 lg:grid-cols-3 lg:gap-10 xl:grid-cols-4'>
             {data.pages.map((page) => {
               return page.results.map((movie: any) => {
@@ -106,17 +166,47 @@ export default function Home(): React.ReactElement {
           </div>
         )}
 
-        {/* 다음 페이지 버튼 */} 
-        {/* 다음 페이지 버튼이 화면에 있을 경우 */}
-        {(isSuccess && hasNextPage && !isFetchingNextPage) && (
+        {/* 다음 페이지 버튼 */}
+        {isSuccess && hasNextPage && !isFetchingNextPage && (
           <LoadMoreButton
             fetchNextPage={fetchNextPage}
             getNextPageButton={getNextPageButton}
           />
         )}
-        {/* 다음 페이지 버튼이 로딩 중일 경우 */}
-        {(isSuccess && isFetchingNextPage) && (
-          <LoadingPing loadMore={true} />
+        {isSuccess && isFetchingNextPage && <LoadingPing loadMore={true} />}
+
+        {/* 닉네임 설정 모달 */}
+        {showModal && (
+          <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
+            <form onSubmit={handleUpdateNickname}>
+              <div className='bg-white rounded-lg shadow-lg p-6 w-80'>
+                <p className='text-lg font-bold mb-4'>
+                  사용할 닉네임을 설정해주세요
+                </p>
+                <input
+                  type='text'
+                  className='border border-gray-300 rounded-lg px-3 py-2 w-full mb-4 focus:outline-none focus:ring focus:border-second'
+                  value={nickname}
+                  onChange={handleNickname}
+                />
+                <div className='flex justify-end'>
+                  <button
+                    type='button'
+                    className='bg-gray-300 text-gray-700 px-4 py-2 rounded-lg mr-2 transition duration-300 ease-in-out hover:bg-gray-400 hover:text-white'
+                    onClick={closeModal}
+                  >
+                    취소
+                  </button>
+                  <button
+                    type='submit'
+                    className='bg-second text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out hover:bg-first'
+                  >
+                    저장
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         )}
       </div>
     </div>
