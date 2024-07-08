398d21756f2342e5655010c3518cf62a58ce0a93
jeong922 <hwajeong922@naver.com>
2024-07-09 04:12:32 +0900

    fix: Spinner size 수정

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,24 +1,21 @@
 interface IProps {
-  size: 'sm' | 'lg';
+  size: 'xs' | 'sm' | 'lg';
   item?: boolean;
 }
 
-export default function Spinner ({
-  size,
-  item = false
-}: IProps) {
-  const spinnerSize: string = size === 'sm' ? '3rem' : '6rem';
+export default function Spinner({ size, item = false }: IProps) {
   const spinner = (
     <div
-      className="mx-auto animate-spin border-t-4 border-r-4 border-l-4 border-b-4 border-t-second border-r-second rounded-full my-2"
-      style={{ height: spinnerSize, width: spinnerSize }}
+      className={`mx-auto animate-spin border-t-4 border-r-4 border-l-4 border-b-4 border-t-second border-r-second rounded-full my-2 ${getSpinnerSize(
+        size
+      )}`}
     ></div>
   );
 
   return (
     <>
       {item ? (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className='flex items-center justify-center min-h-screen'>
           {spinner}
         </div>
       ) : (
@@ -26,4 +23,15 @@ export default function Spinner ({
       )}
     </>
   );
+}
+
+function getSpinnerSize(size: 'xs' | 'sm' | 'lg') {
+  switch (size) {
+    case 'xs':
+      return 'h-8 w-8';
+    case 'sm':
+      return 'h-12 w-12';
+    case 'lg':
+      return 'h-24 w-24';
+  }
 }
