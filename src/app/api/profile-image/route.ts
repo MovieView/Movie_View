073013aa62f59b31c68f2a58dd8748a40f2ab623
choongnyeong6215 <choong6215@gmail.com>
2024-07-10 14:55:58 +0900

    fix : 닉네임 설정 모달 유효성 검증 & 로그인 유저 프로필 이미지 렌더링 방식 수정

---
+++ src/app/api/profile-image/route.ts
@@ -0,0 +1,58 @@
+import { dbConnectionPoolAsync } from "@/lib/db";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const connection = await dbConnectionPoolAsync.getConnection();
+
+    const { userId, provider } = await req.json();
+
+    const formUid = (provider: string) => {
+      switch (provider) {
+        case "github":
+          return "github_" + userId;
+        case "kakao":
+          return "kakao_" + userId;
+        case "google":
+          return "google_" + userId;
+      }
+    };
+
+    const sql = `
+      SELECT JSON_UNQUOTE(JSON_EXTRACT(extra_data, '$.filepath')) AS filepath
+      FROM social_accounts
+      WHERE uid = ?
+    `;
+
+    const [result] = await connection.execute(sql, [formUid(provider)]);
+
+    if (!result) {
+      return new Response(
+        JSON.stringify({ error: "프로필이 존재하지 않습니다." }),
+        {
+          status: 404,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    } else {
+      return new Response(
+        JSON.stringify({
+          message: "프로필 사진 불러오기 완료",
+          filepath: result,
+        }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ error: "프로필 사진 불러오기 실패" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
--- src/components/login/UserProfile.tsx
+++ src/components/login/UserProfile.tsx
@@ -1,29 +1,75 @@
-'use client';
+"use client";
 
-import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const UserProfile = () => {
   const { data: session } = useSession();
+  const [profileImg, setProfileImg] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (session && session.user) {
+      const userId = session.uid;
+      const provider = session.provider;
+
+      const storageKey = `${provider}_profileImg`;
+
+      const cachedProfileImg = localStorage.getItem(storageKey);
+      if (cachedProfileImg) {
+        setProfileImg(cachedProfileImg);
+      } else {
+        // 서버에서 프로필 이미지 가져오기
+        getProfileImg(userId, provider);
+      }
+    }
+  }, [session]);
+
+  const getProfileImg = async (userId: string, provider: string) => {
+    try {
+      const response = await fetch(`/api/profile-image`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId,
+          provider,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to save user");
+      }
+
+      const result = await response.json();
+      const filePath = result.filepath[0]["filepath"];
+      const storageKey = `${provider}_profileImg`;
+      localStorage.setItem(storageKey, filePath);
+      setProfileImg(filePath);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleLogOut = async () => {
-    await signOut({ callbackUrl: '/' });
+    await signOut({ callbackUrl: "/" });
   };
 
   return (
-    <div className='flex justify-between items-center text-xl gap-4'>
+    <div className="flex justify-between items-center text-xl gap-4">
       {session ? (
         <>
           <img
-            src={`${session.user?.image}`}
-            alt={`${session.user?.name}의 프로필 이미지`}
-            className='rounded-full h-16 w-16'
+            src={`${profileImg}`}
+            alt={`${session.user.name}`}
+            className={profileImg ? "rounded-full h-16 w-16" : "bg-transparent"}
           />
           <button onClick={handleLogOut}>LogOut</button>
         </>
       ) : (
         <>
-          <Link href='/login'>
+          <Link href="/login">
             <button>LogIn</button>
           </Link>
         </>
