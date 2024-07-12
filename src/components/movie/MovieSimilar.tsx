010592dd53448d3f97dd97c1db5244f74cadc362
jeong922 <hwajeong922@naver.com>
2024-07-12 15:55:59 +0900

    style: MovieSimilar, RecentReview 슬라이드 z-index 값 삭제

--- src/components/movie/MovieSimilar.tsx
+++ src/components/movie/MovieSimilar.tsx
@@ -88,7 +88,7 @@ const MovieSimilar = ({ similarMovies }: MovieSimilarProps) => {
         {leftBtnVisible && (
           <button
             onClick={handlePrev}
-            className='absolute -left-5 top-1/2 transform -translate-y-1/2 z-10 w-10 h-10 rounded-full bg-white shadow-buttonShadow flex justify-center items-center'
+            className='absolute -left-5 top-1/2 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white shadow-buttonShadow flex justify-center items-center'
           >
             <IoIosArrowBack size={25} />
           </button>
@@ -96,7 +96,7 @@ const MovieSimilar = ({ similarMovies }: MovieSimilarProps) => {
         {rightBtnVisible && (
           <button
             onClick={handleNext}
-            className='absolute -right-5 top-1/2 transform -translate-y-1/2 z-10 w-10 h-10 rounded-full bg-white shadow-buttonShadow flex justify-center items-center'
+            className='absolute -right-5 top-1/2 transform -translate-y-1/2 w-10 h-10 rounded-full bg-white shadow-buttonShadow flex justify-center items-center'
           >
             <IoIosArrowForward size={25} />
           </button>
