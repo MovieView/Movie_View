da762447cc589770e4d5535f525d3303bb460b15
WongilK <wwwkim99@naver.com>
2024-07-12 12:56:44 +0900

    feat: scrollBy로 변경

--- src/components/movie/MovieSimilar.tsx
+++ src/components/movie/MovieSimilar.tsx
@@ -2,9 +2,8 @@
 
 import { SimilarMovie } from '@/models/movie.model';
 import Link from 'next/link';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import { debounce } from '../review/ReviewItem';
 
 interface MovieSimilarProps {
   similarMovies: SimilarMovie[];
@@ -14,92 +13,77 @@ interface MovieCardProps {
 }
 const MovieCard = ({ movie }: MovieCardProps) => {
   return (
-    <div className='flex-shrink-0 w-[calc((100%-36px)/4)] md:w-[calc((100%-48px)/5)] xl:w-[calc((100%-108px)/10)] '>
-      <Link href={`/detail/${movie.id}`}>
-        {movie.poster_path ? (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-            className='w-full h-full object-cover rounded-md'
-            alt={movie.title}
-          />
-        ) : (
-          <div className='bg-slate-500 w-full h-full rounded-md flex items-center justify-center'>
-            {movie.title}
-          </div>
-        )}
-      </Link>
-    </div>
+    <Link href={`/detail/${movie.id}`}>
+      {movie.poster_path ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          className='w-full h-full object-cover rounded-md'
+          alt={movie.title}
+        />
+      ) : (
+        <div className='bg-slate-500 w-full h-full rounded-md flex items-center justify-center'>
+          {movie.title}
+        </div>
+      )}
+    </Link>
   );
 };
 
 const MovieSimilar = ({ similarMovies }: MovieSimilarProps) => {
   const [leftBtnVisible, setLeftBtnVisible] = useState(false);
   const [rightBtnVisible, setRightBtnVisible] = useState(true);
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [itemsPerView, setItemsPerView] = useState(2);
-
-  const handleResize = useCallback(() => {
-    if (window.innerWidth <= 768) {
-      setItemsPerView(4);
-    } else if (window.innerWidth <= 1280) {
-      setItemsPerView(5);
-    } else {
-      setItemsPerView(10);
-    }
-  }, []);
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', debounce(handleResize, 100));
-
-    return () => {
-      window.removeEventListener('resize', debounce(handleResize, 100));
-    };
-  }, [handleResize]);
-
-  useEffect(() => {
-    setLeftBtnVisible(currentIndex > 0);
-    setRightBtnVisible(
-      currentIndex < Math.ceil(similarMovies.length / itemsPerView) - 1
-    );
-  }, [currentIndex, itemsPerView, similarMovies.length]);
-
-  const handleNext = () => {
-    if (currentIndex === Math.ceil(similarMovies.length / itemsPerView) - 1)
-      return;
-    setCurrentIndex((prev) => prev + 1);
-  };
+  const sliderRef = useRef<HTMLDivElement | null>(null);
 
   const handlePrev = () => {
-    if (currentIndex === 0) return;
-    setCurrentIndex((prev) => prev - 1);
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.clientWidth - 12,
+        behavior: 'smooth',
+      });
+    }
   };
 
-  const transformValue = useMemo(() => {
-    if (typeof window !== 'undefined') {
-      return currentIndex * -100 - (18 / window.innerWidth) * 100;
+  const handleNext = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: sliderRef.current.clientWidth + 12,
+        behavior: 'smooth',
+      });
     }
-    return 0;
-  }, [currentIndex]);
+  };
+
+  useEffect(() => {
+    if (sliderRef.current) {
+      const slider = sliderRef.current;
+      const handleScroll = () => {
+        if (sliderRef.current) {
+          const { scrollLeft, scrollWidth, clientWidth } = sliderRef.current;
+          setLeftBtnVisible(scrollLeft > 0);
+          setRightBtnVisible(scrollLeft + 10 < scrollWidth - clientWidth);
+        }
+      };
+      slider.addEventListener('scroll', handleScroll);
+      return () => {
+        slider.removeEventListener('scroll', handleScroll);
+      };
+    }
+  }, []);
 
   return (
     <div className='mx-auto max-w-5xl mt-6 lg:px-8 px-6'>
       <div className='font-bold text-lg mb-3'>비슷한 영화</div>
 
       <div className='relative px-3 py-3 rounded'>
-        <div className='overflow-hidden'>
-          <div
-            className='flex gap-3'
-            style={{
-              transform: `translateX(${transformValue}%)`,
-              transition: 'transform 1s ease-in-out',
-            }}
-          >
-            {similarMovies.map((movie) => (
-              <MovieCard movie={movie} key={movie.id} />
-            ))}
-          </div>
+        <div className='flex gap-3 overflow-x-auto' ref={sliderRef}>
+          {similarMovies.map((movie) => (
+            <div
+              className='flex-shrink-0 w-[calc((100%-36px)/4)] md:w-[calc((100%-48px)/5)] xl:w-[calc((100%-108px)/10)]'
+              key={movie.id}
+            >
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
         {leftBtnVisible && (
           <button
